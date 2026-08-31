7889fa2304712b4404d78c391193732f908de77c
Vladislav Glovatskiy <git13vl@gmail.com>
2026-08-31 23:24:55 +0500

    Refactor: integrate nav into header component

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,37 @@
-import Nav from "./Nav"
 import logo from "./assets/Logo.svg"
+import hamburgerIcon from "./assets/🦆 icon _hamburger menu_.svg"
+
 const Header = () => {
     return (
-        <header className="header">
-            <img src={logo} className="header-logo"/>
-            <Nav/>
-        </header>
-    )
+      <header className="header">
+        <img src={logo} className="header-logo" />
+        <nav>
+          <ul className="nav-items desktop-view">
+            <li>
+              <a href="">HOME</a>
+            </li>
+            <li>
+              <a href="">ABOUT</a>
+            </li>
+            <li>
+              <a href="">MENU</a>
+            </li>
+            <li>
+              <a href="">RESERVATIONS</a>
+            </li>
+            <li>
+              <a href="">ORDER ONLINE</a>
+            </li>
+            <li>
+              <a href="">LOGIN</a>
+            </li>
+          </ul>
+        </nav>
+        <button className="nav-button mobile-view" type="button">
+          <img src={hamburgerIcon} alt="Hamburger menu" />
+        </button>
+      </header>
+    );
 }
 
 export default Header
